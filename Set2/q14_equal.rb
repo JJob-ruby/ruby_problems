51356af8575f9c1ee2b34fd1890ec6e7b3b8439a
JJob <job.jagash@rubyians.com>
2019-12-11 19:06:38 +0530

    Lot of files deleted

---
+++ Set2/q14_equal.rb
@@ -0,0 +1,17 @@
+
+
+a = a = ['10', '2', '30', '5', "abc"]
+
+puts "Checking for character '5'"
+
+puts a.include? '5'
+
+puts "Checking for number 5"
+
+puts a.include? 5
+
+puts "Checking for string 'abc'"
+
+puts a.include? 'abc'
+
+
--- q10_array.rb
+++
@@ -1,9 +0,0 @@
-
-
-arr = Array.new(10)
-
-for i in 0..arr.length-1 do
-	
-	arr[i]=rand(10)
-	print arr[i].to_s+" "
-end
